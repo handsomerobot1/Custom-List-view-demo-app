c7892c9badf23ad608cb9e0b90bab97ebc4c2d92
alamin <alaminjoy456@gmail.com>
2024-10-15 23:42:45 +0600

    removed civ colour bcz circluer img view denpency doesn't have build in attribute  and full project done

--- app/src/main/java/com/example/customlistview/MainActivity.kt
+++ app/src/main/java/com/example/customlistview/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.customlistview
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val name = arrayOf<String>("Large pizza","Extra large pizza","Midium pizza","Small pizza","Small pizza","Small pizza")
+    val descTxt=arrayListOf<String>("nice pizza","very big pizza","very good test","yummy","testy","good")
+    val image = arrayOf<Int>(R.drawable.pizza,R.drawable.pizza,R.drawable.pizza,R.drawable.pizza,R.drawable.pizza,R.drawable.pizza)
+    private lateinit var listView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        listView = findViewById(R.id.listView)
 
+        val listAdapter = ListAdapter(this, name,descTxt,image)
+        listView.adapter = listAdapter
     }
 }
